f2e04a57048c8dda48a89a0e2ac7a85761e9122f
Slava Rybalka <slavarybalko@gmail.com>
2015-03-19 06:44:31 -0500

    added imagemagick, paperclip, updated pins views

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass', '~> 3.3.1.0'
 gem 'devise', '~> 3.4.1'
+gem 'paperclip', '~> 4.2.1'
 
 group :doc do
   gem 'sdoc', require: false
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -47,13 +47,12 @@ class PinsController < ApplicationController
     end
 
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
       redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
-
 
 end
