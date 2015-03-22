900d7ba41e7abb80c4ba834b56a9cf7db3c8378d
Slava Rybalka <slavarybalko@gmail.com>
2015-03-21 19:08:59 -0500

    bucket name in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Pinterested::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
+      :bucket => ENV['slavapins'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
