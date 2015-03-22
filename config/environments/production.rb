acfdb24e3a1bfd96e404cf7d96fbf09105a6ba59
Slava Rybalka <slavarybalko@gmail.com>
2015-03-21 21:09:26 -0500

    changed AWS to S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,11 +82,11 @@ Pinterested::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :bucket => ENV['S3_BUCKET'],
+      :access_key_id => ENV['S3_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
     }
   }
-#test to git
+
 
 end
